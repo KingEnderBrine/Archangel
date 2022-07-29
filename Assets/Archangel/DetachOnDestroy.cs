02c1e8258d1f76ef34eede3b294ef48f19772788
KingEnderBrine <kingenderbrin@gmail.com>
2022-07-29 18:31:35 +0700

    Fix missing references

---
+++ Assets/Archangel/DetachOnDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Archangel
+{
+    public class DetachOnDestroy : MonoBehaviour
+    {
+        public Transform[] targets = Array.Empty<Transform>();
+        public Behaviour[] enableComponents = Array.Empty<Behaviour>();
+        public Behaviour[] disableComponents = Array.Empty<Behaviour>();
+
+        private void OnDestroy()
+        {
+            foreach (var target in targets)
+            {
+                target.SetParent(null, true);
+            }
+
+            foreach (var component in enableComponents)
+            {
+                component.enabled = true;
+            }
+
+            foreach (var component in disableComponents)
+            {
+                component.enabled = false;
+            }
+        }
+    }
+}
